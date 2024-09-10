257aba3359f4ed6cb8ae226ba64882706a60deac
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2024-09-10 15:11:35 +0000

    Sync LeetCode submission - Insert Greatest Common Divisors in Linked List (java)

---
+++ my-folder/problems/insert_greatest_common_divisors_in_linked_list/solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            int gcdValue = gcd(current.val, current.next.val);
+            ListNode newNode = new ListNode(gcdValue, current.next);
+            current.next = newNode;
+            current = newNode.next;
+        }
+        
+        return head;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
