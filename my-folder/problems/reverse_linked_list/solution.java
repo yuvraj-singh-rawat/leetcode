0b1e1cca91dd6a3cf74014187af1ac2b9c398c14
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2025-01-01 09:52:58 +0000

    Sync LeetCode submission - Reverse Linked List (java)

--- my-folder/problems/reverse_linked_list/solution.java
+++ my-folder/problems/reverse_linked_list/solution.java
@@ -10,19 +10,18 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode next = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-            next = curr.next;
-
-            curr.next = prev;
-
-            prev = curr;
-            curr = next;
+        // Base case: if list is empty or has only one node
+        if (head == null || head.next == null) {
+            return head;
         }
 
-        return prev;
+        // Recursively reverse the rest of the list
+        ListNode reversedHead = reverseList(head.next);
+
+        // Reverse the current node
+        head.next.next = head; // Make the next node point to the current node
+        head.next = null; // Break the current node's original next link
+
+        return reversedHead; // Return the new head
     }
 }
