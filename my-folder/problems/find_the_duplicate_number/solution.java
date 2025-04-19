d1dd6d254c330c555d57eac3f9de9042f0716255
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2025-04-19 11:35:13 +0000

    Sync LeetCode submission - Find the Duplicate Number (java)

--- my-folder/problems/find_the_duplicate_number/solution.java
+++ my-folder/problems/find_the_duplicate_number/solution.java
@@ -3,20 +3,16 @@ class Solution {
         int slow = nums[0];
         int fast = nums[0];
 
-        while(true) {
+        do {
             slow = nums[slow];
-            fast = nums[nums[fast]];
+            fast = nums[nums[fast]]; 
+        } while (slow != fast);
 
-            if (slow == fast) {
-                break;
-            }
-        }
+        slow = nums[0];
 
-        int slow2 = nums[0];
-
-        while (slow != slow2) {
+        while(slow != fast) {
             slow = nums[slow];
-            slow2 = nums[slow2];
+            fast = nums[fast];
         }
 
         return slow;
