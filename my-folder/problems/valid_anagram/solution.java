e78ed5462c598591c63bed4a7e932b776d8abd8f
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2025-07-06 11:15:21 +0000

    Sync LeetCode submission - Valid Anagram (java)

---
+++ my-folder/problems/valid_anagram/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return countS.equals(countT);
+    }
+}
