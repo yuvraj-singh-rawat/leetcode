3a0af24d151b8b78717010f7ca297fd5aef731a6
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-01-01 13:50:16 +0000

    Sync LeetCode submission - Plus One (java)

--- my-folder/problems/plus_one/solution.java
+++ my-folder/problems/plus_one/solution.java
@@ -2,15 +2,18 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
 
+        // Traverse from last digit
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
+                digits[i]++;   // just add 1
+                return digits; // done
             }
-            digits[i] = 0;
+            digits[i] = 0; // if 9, make it 0 and carry forward
         }
+
+        // If all digits were 9
         int[] result = new int[n + 1];
-        result[0] = 1;
+        result[0] = 1; // like 999 -> 1000
         return result;
     }
 }
