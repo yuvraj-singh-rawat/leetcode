74835acedaee38a028842ccb6b8015b7866d41fa
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-03-06 16:05:00 +0000

    Sync LeetCode submission - Palindrome Number (java)

--- my-folder/problems/palindrome_number/solution.java
+++ my-folder/problems/palindrome_number/solution.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int rem = 0, rev = 0, temp = x;
+        int lastDigit = 0, revNum = 0;
+        int dup = x;
         while (x > 0) {
-            rem = x % 10;
-            rev = (rev * 10) + rem;
+            lastDigit = x % 10;
+            revNum = (revNum * 10) + lastDigit;
             x = x / 10;
         }
-        if (temp == rev) 
-            return true;
-        else 
-            return false;
+        return dup == revNum;
     }
 }
