8862b727a825869bf95d0c8052c91164da255adb
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-03-12 11:32:55 +0000

    Sync LeetCode submission - Move Zeroes (java)

--- my-folder/problems/move_zeroes/solution.java
+++ my-folder/problems/move_zeroes/solution.java
@@ -1,42 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-
-        int j = nums.length-1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                j = i;
-                break;
-            }
-        }
-
-        for (int i = j+1; i < nums.length; i++) {
+        int n = nums.length;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
             if (nums[i] != 0) {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                j++; 
+                j++;
             }
         }
-
-
-        // Brute Force Approach
-
-        // ArrayList<Integer> temp = new ArrayList<>();
-
-        // for(int i=0; i<nums.length; i++) {
-        //     if (nums[i] != 0) {
-        //         temp.add(nums[i]);
-        //     }
-        // }
-
-        // int tempSize = temp.size();
-
-        // for(int i=0; i <tempSize; i++) {
-        //     nums[i] = temp.get(i);
-        // }
-
-        // for(int i=tempSize; i<nums.length; i++) {
-        //     nums[i] = 0;
-        // }
     }
 }
