0e5d033b5f3c0a1a59464309c0519254b6c140c4
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-04-02 16:13:24 +0000

    Sync LeetCode submission - Rotate String (java)

--- my-folder/problems/rotate_string/solution.java
+++ my-folder/problems/rotate_string/solution.java
@@ -1,65 +1,18 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if (s.length() != goal.length()) return false;
+        // int n = s.length();
 
-        String text = s + s;
-        String pattern = goal;
- 
-        int n = text.length();
-        int m = pattern.length();
+        // for (int i = 0; i < n; i++) {
+        //     if (s.equals(goal)) return true;
 
-        int[] lps = computeLPS(pattern);
+        //     s = s.substring(1) + s.charAt(0);
+        // }
+        // return false;
 
-        int i = 0; 
-        int j = 0; 
-
-        while (i < n) {
-            if (pattern.charAt(j) == text.charAt(i)) {
-                j++;
-                i++;
-            }
-
-            if (j == m) {
-                return true;
-            }
-            else if (i < n && pattern.charAt(j) != text.charAt(i)) {
-                if (j != 0)
-                    j = lps[j - 1];
-                else
-                    i = i + 1;
-            }
+        if (s.length() != goal.length()) {
+            return false;
         }
+        return (s + s).contains(goal);
 
-        return false;
-    }
-
-    private int[] computeLPS(String pattern) {
-        int n = pattern.length();
-        int[] lps = new int[n];
-
-        int len = 0;
-
-        lps[0] = 0;
-
-        int i = 1;
-
-        while (i < n) {
-            if (pattern.charAt(i) == pattern.charAt(len)) {
-                len++;
-                lps[i] = len;
-                i++;
-            } 
-            
-            else {
-                if (len != 0) {
-                    len = lps[len - 1];
-                }         
-                else {
-                    lps[i] = 0;
-                    i++;
-                }
-            }
-        }
-        return lps;
     }
 }
