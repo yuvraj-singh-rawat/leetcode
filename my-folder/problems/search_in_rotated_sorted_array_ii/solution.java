23596b8df193c9bae93849cbd6211be057d6ead6
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-04-12 05:53:26 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array II (java)

--- my-folder/problems/search_in_rotated_sorted_array_ii/solution.java
+++ my-folder/problems/search_in_rotated_sorted_array_ii/solution.java
@@ -1,32 +1,30 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int n = nums.length;
-        int start = 0, end = n - 1;
+        int low = 0;
+        int high = nums.length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            if (nums[mid] == target) 
-                return true;
+            if (nums[mid] == target) return true;   
 
-            if (nums[start] == nums[mid] && nums[mid] == nums[end]) {
-                start = start + 1;
-                end = end - 1;
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
                 continue;
             }
-            
 
-            if (nums[start] <= nums[mid]) {
-                if (target >= nums[start] && target <= nums[mid]) {
-                    end = mid - 1;
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
                 } else {
-                    start = mid + 1;
+                    low = mid + 1;
                 }
             } else {
-                if (target >= nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
                 } else {
-                    end = mid - 1;
+                    high = mid - 1;
                 }
             }
         }
