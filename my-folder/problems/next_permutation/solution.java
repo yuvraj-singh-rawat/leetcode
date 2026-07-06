577eb00f181e215bfff248be911631b8f9e2bb82
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-06 05:24:01 +0000

    Sync LeetCode submission - Next Permutation (java)

--- my-folder/problems/next_permutation/solution.java
+++ my-folder/problems/next_permutation/solution.java
@@ -1,39 +1,42 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int piv = -1;
+
+        int pivot = -1;
 
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                piv = i;
+                pivot = i;
                 break;
             }
         }
 
-        if (piv == -1) {
-            reverse(nums, 0, n - 1);
+        if (pivot == -1) {
+            reverse(nums, 0, n-1);
             return;
         }
 
-        for (int i = n-1; i > piv; i--) {
-            if (nums[i] > nums[piv]) {
-                swap(nums, i, piv);
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                swap(nums, i, pivot);
                 break;
             }
         }
 
-        reverse(nums, piv + 1, n - 1);
+        reverse(nums, pivot + 1, n - 1);
     }
 
-    static void swap(int[] nums, int i, int j) {
+    private static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    static void reverse(int[] nums, int start, int end) {
+    private static void reverse(int[] nums, int start, int end) {
         while (start < end) {
-            swap(nums, start++, end--);
+            swap(nums, start, end);
+            start++;
+            end--;
         }
     }
 }
