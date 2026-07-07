17bef89e047358020d60e985d2f44f63789cf433
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-07 08:47:56 +0000

    Sync LeetCode submission - Rotate Image (java)

--- my-folder/problems/rotate_image/solution.java
+++ my-folder/problems/rotate_image/solution.java
@@ -1,22 +1,14 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-
-        // Transpose the matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
+        
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-
-        // Reverse each row
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-j-1];
-                matrix[i][n-j-1] = temp;
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = temp;
             }
         }
     }
