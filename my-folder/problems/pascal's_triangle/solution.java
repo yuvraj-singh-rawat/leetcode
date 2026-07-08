6cc67a15a8d2abbd76f3d8020983852d7880c6fc
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-08 15:15:54 +0000

    Sync LeetCode submission - Pascal's Triangle (java)

--- my-folder/problems/pascal's_triangle/solution.java
+++ my-folder/problems/pascal's_triangle/solution.java
@@ -1,19 +1,23 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-        if (numRows == 0) return result;
+
+        if (numRows == 0) {
+            return result;
+        }
+
         for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>(i+1);
+            List<Integer> rowNums = new ArrayList<>(i+1);
 
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
-                    row.add(1);
+                    rowNums.add(1);
                 } else {
                     int num = result.get(i-1).get(j) + result.get(i-1).get(j-1);
-                    row.add(num);
+                    rowNums.add(num);
                 }
             }
-            result.add(row);
+            result.add(rowNums);
         }
         return result;
     }
