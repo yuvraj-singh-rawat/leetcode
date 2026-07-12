cf949658e71c58c15eb6e215d4628d4c0a67b00f
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-12 11:14:29 +0000

    Sync LeetCode submission - Majority Element II (java)

--- my-folder/problems/majority_element_ii/solution.java
+++ my-folder/problems/majority_element_ii/solution.java
@@ -1,39 +1,64 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
+
+        int ele1 = -1;
+        int ele2 = -1;
+
         int count1 = 0;
-        int maj1 = 0;
-
         int count2 = 0;
-        int maj2 = 0;
 
+        // 1st Pass: Find potential candidates
         for (int num : nums) {
-            if (num == maj1) {
+
+            // Increment count for candidate 1
+            if (num == ele1) {
                 count1++;
-            } else if (num == maj2) {
+            }
+
+            // Increment count for candidate 2
+            else if (num == ele2) {
                 count2++;
-            } else if (count1 == 0) {
-                maj1 = num;
+            }
+
+            // New candidate 1 if count is zero
+            else if (count1 == 0) {
+                ele1 = num;
                 count1 = 1;
-            } else if (count2 == 0) {
-                maj2 = num;
+            }
+
+            // New candidate 2 if count is zero
+            else if (count2 == 0) {
+                ele2 = num;
                 count2 = 1;
-            } else {
+            }
+
+            // Decrease counts if neither candidate matches
+            else {
                 count1--;
                 count2--;
             }
         }
 
+        // 2nd Pass: Verify actual counts
         count1 = 0;
         count2 = 0;
         for (int num : nums) {
-            if (num == maj1) count1++;
-            else if (num == maj2) count2++;
+            if (num == ele1) {
+                count1++;
+            } else if (num == ele2) {
+                count2++;
+            }
         }
 
+        // Prepare result list
         List<Integer> result = new ArrayList<>();
-        if (count1 > n/3) result.add(maj1);
-        if (count2 > n/3) result.add(maj2);
+        if (count1 > n / 3) {
+            result.add(ele1);
+        }
+        if (count2 > n / 3 && ele1 != ele2) {
+            result.add(ele2);
+        }
 
         return result;
     }
