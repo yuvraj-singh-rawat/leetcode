bd52f18d5e83a68899b45dd5f395fd11033223ed
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-13 12:31:57 +0000

    Sync LeetCode submission - 3Sum (java)

--- my-folder/problems/3sum/solution.java
+++ my-folder/problems/3sum/solution.java
@@ -1,11 +1,15 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
+
         List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
             int left = i + 1;
             int right = n - 1;
@@ -13,17 +17,23 @@ class Solution {
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if (sum == 0) {
+                if (sum < 0) {
+                    left++;
+                }
+                else if (sum > 0) {
+                    right--;
+                }
+                else {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    while (left < right && nums[left] == nums[left+1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
 
                     left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
                     right--;
                 }
             }
