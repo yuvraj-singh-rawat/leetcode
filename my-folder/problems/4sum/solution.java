ce0983244040ea913f8905f1c85f101ca90bcd2c
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-13 13:33:17 +0000

    Sync LeetCode submission - 4Sum (java)

--- my-folder/problems/4sum/solution.java
+++ my-folder/problems/4sum/solution.java
@@ -1,20 +1,22 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
+
         List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < n - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
                 int left = j + 1;
                 int right = n - 1;
 
                 while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right]; 
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
 
                     if (sum == target) {
                         result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
@@ -24,14 +26,12 @@ class Solution {
 
                         left++;
                         right--;
-                    } else if (sum < target) {
-                        left++;
-                    } else {
-                        right--;
-                    }
-                }
+                    } 
+
+                    else if (sum < target) left++;
+                    else right--;
+                } 
             }
-            
         }
         return result;
     }
