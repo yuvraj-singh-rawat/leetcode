3be38f907f479ba741fe355c786e0f6334404655
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-19 09:19:24 +0000

    Sync LeetCode submission - Isomorphic Strings (java)

--- my-folder/problems/isomorphic_strings/solution.java
+++ my-folder/problems/isomorphic_strings/solution.java
@@ -1,24 +1,13 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
 
-        int n = s.length();
+        for (int i = 0; i < s.length(); i++) {
+            if (mapS[s.charAt(i)] != mapT[t.charAt(i)]) return false;
 
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            if (!map1.containsKey(s.charAt(i))) {
-                map1.put(s.charAt(i), i);
-            }
-
-            if (!map2.containsKey(t.charAt(i))) {
-                map2.put(t.charAt(i), i);
-            }
-
-            if (!map1.get(s.charAt(i)).equals(map2.get(t.charAt(i)))) {
-                return false;
-            }
+            mapS[s.charAt(i)] = i + 1;
+            mapT[t.charAt(i)] = i + 1;
         }
         return true;
     }
