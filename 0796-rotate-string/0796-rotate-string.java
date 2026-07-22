dbd3c8342b2eed4cdd60e48389afe83101f4e2bd
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-22 11:49:16 +0530

    Time: 2 ms (40.3%), Space: 43.4 MB (29.53%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        // Edge Case
-        if (s.length() != goal.length())
-            return false;
+        if (s.length() != goal.length()) return false;
 
-        // double the string
         String doubleS = s + s;
 
-        // check if goal is in doubles
         return doubleS.contains(goal);
     }
 }
