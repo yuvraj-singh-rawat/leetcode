b48d2931cad926b504a594dfd079085ba696d132
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-22 06:19:12 +0000

    Sync LeetCode submission - Rotate String (java)

--- my-folder/problems/rotate_string/solution.java
+++ my-folder/problems/rotate_string/solution.java
@@ -1,18 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        // int n = s.length();
+        if (s.length() != goal.length()) return false;
 
-        // for (int i = 0; i < n; i++) {
-        //     if (s.equals(goal)) return true;
+        String doubleS = s + s;
 
-        //     s = s.substring(1) + s.charAt(0);
-        // }
-        // return false;
-
-        if (s.length() != goal.length()) {
-            return false;
-        }
-        return (s + s).contains(goal);
-
+        return doubleS.contains(goal);
     }
 }
