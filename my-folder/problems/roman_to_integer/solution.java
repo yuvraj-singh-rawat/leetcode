519779c2a9beac351b7f1a1662f34bfc308268ea
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-07-23 03:14:05 +0000

    Sync LeetCode submission - Roman to Integer (java)

--- my-folder/problems/roman_to_integer/solution.java
+++ my-folder/problems/roman_to_integer/solution.java
@@ -1,25 +1,28 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
         int result = 0;
-
         for (int i = 0; i < s.length(); i++) {
-            int value = map.get(s.charAt(i));
-
-            if (i < s.length() - 1 && value < map.get(s.charAt(i + 1))) {
-                result -= value;
+            int current = value(s.charAt(i));
+            
+            if (i < s.length() - 1 && current < value(s.charAt(i + 1))) {
+                result -= current;
             } else {
-                result += value;
+                result += current;
             }
         }
         return result;
+    }
+
+    private int value(char ch) {
+        switch(ch) {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default: return 0;
+        }
     }
 }
