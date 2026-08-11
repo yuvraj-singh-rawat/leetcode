2e3dd2edebb679bf2dcf15f5d2f01cb6788aefc5
Yuvraj Singh <127573114+yuvraj-singh-rawat@users.noreply.github.com>
2026-08-11 23:20:00 +0530

    Time: 7 ms (99.91%), Space: 45.7 MB (14.59%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -3,13 +3,37 @@ class Solution {
         Arrays.sort(nums);
         int closest = nums[0] + nums[1] + nums[2];
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
             int right = nums.length - 1;
 
+            // Pruning 1: Smallest possible sum for current 'i'
+            int minSum = nums[i] + nums[left] + nums[left + 1];
+            if (minSum > target) {
+                if (Math.abs(minSum - target) < Math.abs(closest - target)) {
+                    closest = minSum;
+                }
+                break;
+            }
+
+            // Pruning 2: Largest possible sum for current 'i'
+            int maxSum = nums[i] + nums[right - 1] + nums[right];
+            if (maxSum < target) {
+                if (Math.abs(maxSum - target) < Math.abs(closest - target)) {
+                    closest = maxSum;
+                }
+                continue;
+            }
+
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == target) {
+                    return sum;
+                }
 
                 if (Math.abs(target - sum) < Math.abs(target - closest)) {
                     closest = sum;
